7998fc1384e480dd896a153f8bbf80d95166d351
User227811 <dokinovfedor@gmail.com>
2024-02-20 10:15:37 +0700

    1

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -5,11 +5,11 @@ console.log("Hello," + name + "!");
 console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
 let random = Math.floor(((Math.random() * 1000)));
 let rounded = Math.sqrt(random);
-//let random1 = Math.floor(((Math.random() * 5)));
-let random1 = 4;
+let random1 = Math.floor(((Math.random() * 5)));
 let array = [];
 let win = 0;
 let examination = 0;
+let examenation1 = 0;
 function isPrime(num) {
   for (let i = 2; i < num; i++) {
     if (num % i === 0) return false;
@@ -22,13 +22,17 @@ function printPrimes(max) {
     if (isPrime(i));
   }
 }
-for(let z2 = 0; z2 < 3; z2++){
+for(let z2 = 0; z2 <= 3; z2++){
     while( isPrime(rounded) !== true){
         random = Math.floor(Math.random() * 1000);
         random1 = Math.floor(((Math.random() * 1000)));
+        win = win + 1;
     }
+    if(win === 3){
+      console.log('Congratulations ' + name + '!');
+      break;
+  }
     console.log('Question: ' + random1);
-    console.log(isPrime(random1));
     let number = readlineSync.question("Your answer: ");
     if (number === 'yes'){
       examination = true;
@@ -36,38 +40,30 @@ for(let z2 = 0; z2 < 3; z2++){
     if (number === 'no'){
       examination = false;
     }
-    if (number === 'yes' && examination === isPrime(random1)){
-        console.log(random1);
+    if (examination === isPrime(random1)){
         console.log('Correct!');
         win = win + 1;
         random1 = Math.floor(((Math.random() * 1000)));
         continue;
     }
-    if (number === 'yes' && examination !== isPrime(random1)){
-        console.log(random1);
+    if (examination !== isPrime(random1)){
         console.log('Question: ' + random1);
         console.log('your answer: ' + number);
         console.log('\'' + number+ '\' is wrong answer ;(. Correct answer was \'no\'.');
         console.log('Let\'s try again, ' + name + '!');
         break;
     }
-    if (number === 'no' && examination !== isPrime(random1)){
-      console.log(random1);
+    if (examination !== isPrime(random1)){
       console.log('Correct!');
       win = win + 1;
       random1 = Math.floor(((Math.random() * 1000)));
       continue;
     }
-    if (number === 'no' && examination === isPrime(random1)){
-      console.log(random1);
+    if (examination === isPrime(random1)){
       console.log('Question: ' + random1);
       console.log('your answer: ' + number);
       console.log('\'' + number+ '\' is wrong answer ;(. Correct answer was \'yes\'.');
       console.log('Let\'s try again, ' + name + '!');
       break;
-  }
-    if(win === 3){
-        console.log('Congratulations ' + name + '!');
     }
-    random1 = Math.floor(((Math.random() * 1000)));
 }
