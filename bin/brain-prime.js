8d409498aa65e5e288b5b071f4bc339d18848da5
User227811 <dokinovfedor@gmail.com>
2024-05-08 10:41:54 +0700

    12

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -6,10 +6,10 @@ time();
 console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
 let random = Math.floor(((Math.random() * 1000)));
 let rounded = Math.sqrt(random);
-let random1 = Math.floor(((Math.random() * 5)));
+let random1 = Math.floor(((Math.random() * 1000)));
 let win = 0;
 let examination = 0;
-function isPrime(num) {
+/* function isPrime(num) {
   for (let i = 2; i < num; i++) {
     if (num % i === 0) return false;
   }
@@ -23,8 +23,8 @@ function printPrimes(max) {
 }
 for(let z2 = 0; z2 <= 3; z2++){
     while( isPrime(rounded) !== true){
-        random = Math.floor(Math.random() * 1000);
-        random1 = Math.floor(((Math.random() * 1000)));
+        random = 173; //Math.floor(Math.random() * 1000);
+        random1 = 173; //Math.floor(((Math.random() * 1000)));
         win = win + 1;
     }
     if(win === 3){
@@ -71,4 +71,48 @@ for(let z2 = 0; z2 <= 3; z2++){
       console.log('Let\'s try again, ' + name + '!');
       break;
     }
+}*/
+let numbers = 0;
+for(let z2 = 0; z2 <= 3; z2++){
+  random = Math.floor(Math.random() * 1000);
+  random1 = Math.floor(((Math.random() * 1000)));
+  win = win + 1;
+  if(win === 4){
+    console.log('Congratulations, ' + name + '!');
+    break;
+  }
+  for(let z = 2; z < random1; z++){
+    if(random1 % z === 0){
+      numbers = 1;
+    }
+  }
+  if(numbers === 1){
+    console.log("Не простое число");
+    console.log('Question: ' + random1);
+    let number2 = readlineSync.question("Your answer: ");
+    if (number2 === 'yes'){
+      console.log('Question: ' + random1);
+      console.log('your answer: ' + number2);
+      console.log('\'' + number2+ '\' is wrong answer ;(. Correct answer was \'yes\'.');
+      console.log('Let\'s try again, ' + name + '!');
+      break;
+    }
+    if (number2 === 'no'){
+      examination = false;
+    }
+  }else{
+    console.log('Число простое');
+    console.log('Question: ' + random1);
+    let number1 = readlineSync.question("Your answer: ");
+    if (number1 === 'yes'){
+      examination = false;
+    }
+    if (number1 === 'no'){
+      console.log('Question: ' + random1);
+      console.log('your answer: ' + number1);
+      console.log('\'' + number1+ '\' is wrong answer ;(. Correct answer was \'yes\'.');
+      console.log('Let\'s try again, ' + name + '!');
+      break;
+    }
+  }
 }
