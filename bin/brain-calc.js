88ae9d300d183eeea117b4ed870bdd37c2b2aac4
User227811 <dokinovfedor@gmail.com>
2024-05-08 11:58:06 +0700

    12

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-import readlineSync from "readline-sync";
-import time from "./brain-games.js";
+import readlineSync from "readline-sync" ;
+import time from "./brain-games.js" ;
 import { name } from "../src/cli.js";
 time();
 console.log("What is the result of the expression?");
